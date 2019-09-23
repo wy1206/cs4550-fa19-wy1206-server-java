cfa8080b42fb61fc2f8bcc4267f9cbc829f1e18c
wy1206 <33647448+wy1206@users.noreply.github.com>
2019-09-23 19:13:29 -0400

    user render commit

--- src/main/webapp/admin/user-admin.component.client.js
+++ src/main/webapp/admin/user-admin.component.client.js
@@ -7,13 +7,52 @@
     var userService = new AdminUserServiceClient();
     $(main);
 
-    function main() { … }
-    function createUser() { … }
-    function findAllUsers() { … }
-    function findUserById() { … }
-    function deleteUser() { … }
-    function selectUser() { … }
-    function updateUser() { … }
-    function renderUser(user) { … }
-    function renderUsers(users) { … }
+    function main() {
+        $usernameFld = $("#usernameFld");
+        $passwordFld = $("#passwordFld");
+        $firstNameFld = $("#firstNameFld");
+        $lastNameFld = $("#lastNameFld");
+        $createBtn = $(".wbdv-create");
+        $editBtn = $(".wbdv-edit");
+        $removeBtn= $(".wbdv-remove");
+        $userRowTemplate = $("#template");
+        $tbody = $(".wbdv-tbody");
+
+        $createBtn.onclick(createUser());
+        $removeBtn.onclick(deleteUser());
+        $editBtn.onclick(updateUser());
+
+        userService
+            .findAllUsers()
+            .then(renderUsers)
+
+    }
+
+    function createUser() {
+        var username = $usernameFld.value;
+        var password = $passwordFld.value;
+        var user = {$usernameFld}
+
+        userService
+            .createUser(user)
+            .then(renderUser);
+    }
+    function findAllUsers() { }
+    function findUserById() {  }
+    function deleteUser() {  }
+    function selectUser() {  }
+    function updateUser() {  }
+    function renderUser(user) {  }
+    function renderUsers(users) {
+        $tbody.empty();
+        for(var u in users) {
+            const user = users[u];
+            const rowClone = $userRowTemplate.clone();
+            rowClone.removeClass('wbdv-hidden');
+            rowClone.find('.wbdv-username').html(user.username);
+            rowClone.find('.wbdv-password').html(user.password);
+            $tbody.append(rowClone);
+            console.log($tbody)
+        }
+    }
 })();
--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -4,11 +4,33 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001248102/users';
     var self = this;
-    function createUser(user, callback) { … }
-    function findAllUsers(callback) { … }
-    function findUserById(userId, callback) { … }
-    function updateUser(userId, user, callback) { … }
-    function deleteUser(userId, callback) { … }
+    function createUser(user) {
+        return fetch('https://wbdv-generic-server.herokuapp.com/api/001248102/users', {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(function(response){
+            return response.json()
+        })
+    }
+
+    function findAllUsers() {
+        return fetch('https://wbdv-generic-server.herokuapp.com/api/001248102/users')
+            .then(function(response){
+                return response.json()
+            })
+    }
+    function findUserById(userId, callback) {
+
+    }
+    function updateUser(userId, user, callback) {
+
+    }
+    function deleteUser(userId, callback) {
+
+    }
 }
